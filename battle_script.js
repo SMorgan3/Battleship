974625a2f09c9a1a1270b6112987e4155dac4f67
SMorgan3 <smorgan3@cocc.edu>
2017-10-05 10:54:24 -0700

    added hit/miss alerts

--- battle_script.js
+++ battle_script.js
@@ -17,12 +17,15 @@ while (isSunk == false) {
 
         if (guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
+            alert("HIT!");
 
             if (hits == 3) {
                 isSunk = true;
 
                 alert("You sank my battleship!");
             }
+        } else {
+            alert("MISS!");
         }
     }
 }
